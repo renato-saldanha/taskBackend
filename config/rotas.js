0f472697f3d837e422e711bc6f4d361114a06271
Renato Saldanha <ranalisesaldanha@gmail.com>
2022-04-20 17:53:28 -0400

    aeee

--- config/rotas.js
+++ config/rotas.js
@@ -1,17 +1,20 @@
-module.exports = app => {
-  app.post('/cadastrar', app.api.usuario.gravar);
-  app.post('/logar', app.api.auth.logar);
+module.exports = (app) => {
+  app.post("/cadastrar", app.api.usuario.gravar);
+  app.post("/logar", app.api.auth.logar);
 
-  app.route('/tasks')
-  .all(app.config.passport.authenticate())
-  .get(app.api.task.listarTasks)
-  .post(app.api.task.gravar)
+  app
+    .route("/tasks")
+    .all(app.config.passport.authenticate())
+    .get(app.api.task.listarTasks)
+    .post(app.api.task.gravar);
 
-  app.route('/tasks/:id')
-  .all(app.config.passport.authenticate())
-  .delete(app.api.task.deletar)
+  app
+    .route("/tasks/:id")
+    .all(app.config.passport.authenticate())
+    .delete(app.api.task.deletar);
 
-  app.route('/tasks/:id/alterar')
-  .all(app.config.passport.authenticate())
-  .put(app.api.task.alterarTaks)
-}
+  app
+    .route("/tasks/:id/alterar")
+    .all(app.config.passport.authenticate())
+    .put(app.api.task.alterarTaks);
+};
